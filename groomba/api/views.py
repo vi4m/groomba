bb643e28d73595fded80f8b9da9758687ae1f4bd
Kamil Warguła <kwargula@gmail.com>
2015-06-18 15:05:12 +0200

    Add create event resource

--- groomba/api/views.py
+++ groomba/api/views.py
@@ -4,14 +4,12 @@ from datetime import datetime
 from django.views.generic import View
 from django.http import JsonResponse
 
-from groomba.gcalendar.tasks import get_available_rooms
+from groomba.gcalendar.tasks import get_available_rooms, create_event
 
 
 class AvailableRoomsView(View):
-    """Records the current user's interest in an author."""
 
     def get(self, request, *args, **kwargs):
-
         start_string = kwargs.get('start')
         end_string = kwargs.get('end')
         start = iso8601.parse_date(start_string)
@@ -19,3 +17,20 @@ class AvailableRoomsView(View):
         rooms = get_available_rooms(start, end)
         data = {'rooms': rooms}
         return JsonResponse(data)
+
+
+class CreateEventView(View):
+
+    def post(self, request, *args, **kwargs):
+        start_string = self.request.POST.get('start')
+        end_string = self.request.POST.get('end')
+        room_id = self.request.POST.get('room_id')
+        summary = self.request.POST.get('summary')
+        description = self.request.POST.get('description')
+        start = iso8601.parse_date(start_string)
+        end = iso8601.parse_date(end_string)
+
+        event = create_event(
+            start, end, room_id, summary, description
+        )
+        return JsonResponse(event)
