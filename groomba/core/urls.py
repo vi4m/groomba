128aab9f9a48883c55997dccb86ca0c547ebce47
Kamil Warguła <kwargula@gmail.com>
2015-06-18 17:37:38 +0200

    Add row forms book

--- groomba/core/urls.py
+++ groomba/core/urls.py
@@ -3,11 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from groomba.core.views import HomePageView
+from groomba.core.views import HomePageView, FreeRoomsView
 
 urlpatterns = patterns(
     '',
     url(
         r'^start/', HomePageView.as_view(), name='homepage',
     ),
+    url(
+        r'^free-rooms/', FreeRoomsView.as_view(), name='free-rooms',
+    )
 )
